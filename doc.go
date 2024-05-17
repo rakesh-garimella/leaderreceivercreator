b18ec9aa7a60aed1432b5143a57174d2d15c57d6
Stanislav Khalash <stanislav.khalash@sap.com>
2024-05-17 20:54:39 +0200

    Rename pkg

--- doc.go
+++ doc.go
@@ -4,4 +4,4 @@
 //go:generate mdatagen metadata.yaml
 
 // Package receivercreator implements receiver_creator that can instantiate other receivers at runtime.
-package receivercreator // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/receivercreator"
+package leaderelectionreceiver
