b77f8371a85699d69bce96928273e94deef1e60d
Stanislav Khalash <stanislav.khalash@sap.com>
2024-05-18 10:58:21 +0200

    WIP: implement leader election

---
+++ leader_elector.go
@@ -0,0 +1,90 @@
+package leaderelectionreceiver
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	"context"
+)
+
+const (
+	inClusterNamespacePath = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+	defaultLeaseDuration   = 15 * time.Second
+	defaultRenewDeadline   = 10 * time.Second
+	defaultRetryPeriod     = 2 * time.Second
+)
+
+// NewResourceLock creates a new leases resource lock for use in a leader election loop
+func newResourceLock(client kubernetes.Interface, leaderElectionNamespace, lockName string) (resourcelock.Interface, error) {
+	var err error
+	if leaderElectionNamespace == "" {
+		leaderElectionNamespace, err = getInClusterNamespace()
+		if err != nil {
+			return nil, fmt.Errorf("unable to find leader election namespace: %w", err)
+		}
+	}
+
+	// Leader id, needs to be unique, use pod name in kubernetes case.
+	id, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
+	return resourcelock.New(resourcelock.LeasesResourceLock,
+		leaderElectionNamespace, lockName,
+		client.CoreV1(),
+		client.CoordinationV1(),
+		resourcelock.ResourceLockConfig{
+			Identity: id,
+		})
+}
+
+func getInClusterNamespace() (string, error) {
+	// Check whether the namespace file exists.
+	// If not, we are not running in cluster so can't guess the namespace.
+	_, err := os.Stat(inClusterNamespacePath)
+	if os.IsNotExist(err) {
+		return "", fmt.Errorf("not running in-cluster, unable to get namespace")
+	} else if err != nil {
+		return "", fmt.Errorf("error checking namespace file: %w", err)
+	}
+
+	// Load the namespace file and return its content
+	namespace, err := os.ReadFile(inClusterNamespacePath)
+	if err != nil {
+		return "", fmt.Errorf("error reading namespace file: %w", err)
+	}
+	return string(namespace), nil
+}
+
+// NewLeaderElector return  a leader elector object using client-go
+func NewLeaderElector(
+	client kubernetes.Interface,
+	onStartedLeading func(context.Context),
+	onStoppedLeading func(),
+) (*leaderelection.LeaderElector, error) {
+	namespace := "default"
+	lockName := "lock"
+
+	resourceLock, err := newResourceLock(client, namespace, lockName)
+	if err != nil {
+		return &leaderelection.LeaderElector{}, err
+	}
+
+	leConfig := leaderelection.LeaderElectionConfig{
+		Lock:          resourceLock,
+		LeaseDuration: defaultLeaseDuration,
+		RenewDeadline: defaultRenewDeadline,
+		RetryPeriod:   defaultRetryPeriod,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: onStartedLeading,
+			OnStoppedLeading: onStoppedLeading,
+		},
+	}
+
+	return leaderelection.NewLeaderElector(leConfig)
+}
--- receiver.go
+++ receiver.go
@@ -4,11 +4,18 @@
 package leaderelectionreceiver
 
 import (
+	"fmt"
 	"context"
+	"os"
+	"path/filepath"
+
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
 	"go.uber.org/zap"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var _ receiver.Metrics = (*receiverCreator)(nil)
@@ -38,28 +45,65 @@ func (rc *receiverCreator) Start(ctx context.Context, host component.Host) error
 	ctx = context.Background()
 	ctx, rc.cancel = context.WithCancel(ctx)
 
-	go func() {
-		for _, template := range rc.cfg.receiverTemplates {
-			rc.params.TelemetrySettings.Logger.Info("starting receiver",
-				zap.String("name", template.id.String()))
+	client, err := rc.newClient()
+	if err != nil {
+		return fmt.Errorf("failed to create Kubernetes client: %w", err)
+	}
 
-			runner := newReceiverRunner(rc.params, rc.host)
-			_, err := runner.start(
-				receiverConfig{
-					id:     template.id,
-					config: template.config,
-				},
-				rc.nextLogsConsumer,
-				rc.nextMetricsConsumer,
-				rc.nextTracesConsumer,
-			)
-			if err != nil {
-				rc.params.TelemetrySettings.Logger.Error("failed to start receiver", zap.String("receiver", template.id.String()), zap.Error(err))
-				continue
-			}
+	if _, err := NewLeaderElector(
+		client,
+		func(ctx context.Context) {
+			rc.params.TelemetrySettings.Logger.Info("Elected as leader")
+		},
+		func() {
+			rc.params.TelemetrySettings.Logger.Info("Lost leadership")
+		},
+	); err != nil {
+		return fmt.Errorf("failed to create leader elector: %w", err)
+	}
+
+	return nil
+}
+
+func (rc *receiverCreator) newClient() (kubernetes.Interface, error) {
+	kubeConfigPath := filepath.Join(os.Getenv("HOME"), ".kube/config")
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		rc.params.TelemetrySettings.Logger.Warn("Cannot find in cluster config", zap.Error(err))
+		config, err = clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		if err != nil {
+			rc.params.TelemetrySettings.Logger.Error("Cannot build ClientConfig", zap.Error(err))
+			return nil, err
 		}
-	}()
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		rc.params.TelemetrySettings.Logger.Error("Cannot create Kubernetes client", zap.Error(err))
+		return nil, err
+	}
+	return client, nil
+}
 
+func (rc *receiverCreator) startReceiverRunner() error {
+	for _, template := range rc.cfg.receiverTemplates {
+		rc.params.TelemetrySettings.Logger.Info("starting receiver",
+			zap.String("name", template.id.String()))
+
+		runner := newReceiverRunner(rc.params, rc.host)
+		_, err := runner.start(
+			receiverConfig{
+				id:     template.id,
+				config: template.config,
+			},
+			rc.nextLogsConsumer,
+			rc.nextMetricsConsumer,
+			rc.nextTracesConsumer,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to start receiver %s: %w", template.id.String(), err)
+		}
+	}
 	return nil
 }
 
