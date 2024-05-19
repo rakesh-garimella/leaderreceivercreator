f040d3805c8986820740b58e3443aeef6100eb1c
Stanislav Khalash <stanislav.khalash@sap.com>
2024-05-19 20:46:26 +0200

    Remove unneeded code

--- leader_elector.go
+++ leader_elector.go
@@ -1,7 +1,6 @@
 package leaderreceivercreator
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -20,45 +19,21 @@ const (
 
 // NewResourceLock creates a new leases resource lock for use in a leader election loop
 func newResourceLock(client kubernetes.Interface, leaderElectionNamespace, lockName string) (resourcelock.Interface, error) {
-	var err error
-	if leaderElectionNamespace == "" {
-		leaderElectionNamespace, err = getInClusterNamespace()
-		if err != nil {
-			return nil, fmt.Errorf("unable to find leader election namespace: %w", err)
-		}
-	}
-
 	// Leader id, needs to be unique, use pod name in kubernetes case.
 	id, err := os.Hostname()
 	if err != nil {
 		return nil, err
 	}
 
-	return resourcelock.New(resourcelock.LeasesResourceLock,
-		leaderElectionNamespace, lockName,
+	return resourcelock.New(
+		resourcelock.LeasesResourceLock,
+		leaderElectionNamespace,
+		lockName,
 		client.CoreV1(),
 		client.CoordinationV1(),
 		resourcelock.ResourceLockConfig{
 			Identity: id,
 		})
-}
-
-func getInClusterNamespace() (string, error) {
-	// Check whether the namespace file exists.
-	// If not, we are not running in cluster so can't guess the namespace.
-	_, err := os.Stat(inClusterNamespacePath)
-	if os.IsNotExist(err) {
-		return "", fmt.Errorf("not running in-cluster, unable to get namespace")
-	} else if err != nil {
-		return "", fmt.Errorf("error checking namespace file: %w", err)
-	}
-
-	// Load the namespace file and return its content
-	namespace, err := os.ReadFile(inClusterNamespacePath)
-	if err != nil {
-		return "", fmt.Errorf("error reading namespace file: %w", err)
-	}
-	return string(namespace), nil
 }
 
 // newLeaderElector return  a leader elector object using client-go
